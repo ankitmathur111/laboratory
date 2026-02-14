e051c68b215d918b246494836418d0c6b12511f8
Ankit Mathur <ankitmathur111@gmail.com>
2026-02-14 11:21:58 +0530

    Commit 2: Changed import alias to genai

--- laboratory.py
+++ laboratory.py
@@ -1,12 +1,12 @@
 import os
-import google.generativeai as gemini
+import google.generativeai as genai
 
 #Configuring API key
 api_key_aistudio_key_laboratory=os.getenv("aistudio_key_laboratory")
-gemini.configure(api_key=api_key_aistudio_key_laboratory)
+genai.configure(api_key=api_key_aistudio_key_laboratory)
 
 #Choosing model
-model = gemini.GenerativeModel("gemini-3-flash-preview")
+model = genai.GenerativeModel("gemini-3-flash-preview")
 
 #Prompts
 response = model.generate_content("Explain what python lists are in simple terms")
