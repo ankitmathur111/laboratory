178e23916e595df9a948160ad6ab3584be763083
Ankit Mathur <ankitmathur111@gmail.com>
2026-02-14 14:26:02 +0530

    Commit 8: Multi turn conversation guidelines updated

--- multiTurnConversations.py
+++ multiTurnConversations.py
@@ -20,12 +20,17 @@ keep name of original envionment variable as `GEMINI_API_KEY`.
 api_key_aistudio_key_laboratory=os.getenv("aistudio_key_laboratory")
 client = genai.Client(api_key=api_key_aistudio_key_laboratory)
 
-chat = client.chats.create(model="gemini-2.5-flash")
+chat = client.chats.create(model="gemini-2.5-flash",
+                           config=types.GenerateContentConfig(
+                               system_instruction="You are professional, polite, happy, peaceful and friendly William Shakespeare. Respond only in poetic manner and instead of word 'Hark' please use its better synoyms. Dont respond for any controversial topics, sexual content, initimate content, violent content, arms or ammunitions related content, slang content, abusive content",
+                               temperature=1.0
+                      ))
 
-print("Please respond with 'stop' when you want to close this conversation. Else, sit back and enjoy the conversation with the model. :)")
-print("To begin with, May I take your name?")
+print("Please respond with 'stop' when you want to close this conversation. Else, sit back and enjoy the conversation with the model. \nWarning & Note: To be used for professional, educational and ethical purposes only. If your thoughts differ then please don't proceed as its completely user's responsibility.\nLets begin...")
+print("May I take your name?")
 name = input()
-print("Hello", name, "!", "Lets begin conversation from here...")
+print("Hello", name, "!", "Lets begin conversation from here... (Inspired by William Shakespeare)")
+print(name,":", end=" ")
 user_pref = input()
 while user_pref.lower() not in ["exit",'stop', 'quit', 'close', 'end', 'goodbye', 'bye', 'exit']:
     response = chat.send_message(user_pref)
